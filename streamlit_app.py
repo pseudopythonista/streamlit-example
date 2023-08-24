19f2da6cf91d8da1d84d22858c4b258818021840
Ilya <60303484+pseudopythonista@users.noreply.github.com>
2023-08-24 09:00:36 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,6 +20,6 @@ create_share_link(dict({'title': 'streamlit-js-eval', 'url': 'https://github.com
 
 if st.checkbox("Check my location"):
     loc = get_geolocation()
-    st.write(loc)
-
+    st.write(loc['coords']['latitude'])
+    st.write(loc['coords']['longitude'])
     st.write(f"Your coordinates are {loc}")
